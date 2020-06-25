af76be3394a921121cb52fd2a293ad320b734506
james wu <jwu_nyc@yahoo.com>
2020-06-25 20:59:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,13 +7,11 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  sum=num1/num2
-  p sum
+  p num1/num2
 end
 
 def multiplication(num1, num2)
-  sum=num1*num2
-  p sum
+  p num1*num2
 end
 
 def modulo(num1, num2)
